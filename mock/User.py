caffecdb2be449d81ce32c0e6c8534b5a31ba6b0
ajarombek <ajarombek95@gmail.com>
2021-03-13 21:29:00 -0500

    refactor local auth functions to make them closer to production functionality

---
+++ mock/User.py
@@ -0,0 +1,20 @@
+"""
+Model object for the 'user' table found in RDS/MySQL.
+Author: Andrew Jarombek
+Date: 3/13/2021
+"""
+
+from sqlalchemy import Column, VARCHAR
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    username = Column(VARCHAR(20), primary_key=True)
+    first = Column(VARCHAR(30), nullable=False, index=True)
+    last = Column(VARCHAR(30), nullable=False, index=True)
+    password = Column(VARCHAR(255), nullable=False)
+    email = Column(VARCHAR(50), index=True)
--- mock/main.py
+++ mock/main.py
@@ -5,8 +5,16 @@ Date: 10/10/2020
 """
 
 import os
+import re
+from datetime import datetime
 
+import jwt
+import bcrypt
 from flask import Flask, request, jsonify
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from User import User
 
 app = Flask(__name__)
 
@@ -21,7 +29,21 @@ def authenticate():
     data: dict = request.get_json()
     jwt_token = data.get('token')
 
-    return jsonify({'result': jwt_token == 'j.w.t'})
+    try:
+        jwt.decode(
+            jwt=jwt_token,
+            key=public_key,
+            verify=True,
+            algorithms='RS256',
+            options={'require': ['exp', 'iat', 'iss']}
+        )
+        result = True
+    except jwt.ExpiredSignatureError:
+        result = False
+    except Exception:
+        result = False
+
+    return jsonify({'result': result})
 
 
 @app.route('/token', methods=['POST'])
@@ -30,12 +52,48 @@ def token():
     client_id = data.get('clientId')
     client_secret = data.get('clientSecret')
 
-    expected_id = os.getenv('SXCTF_AUTH_ID')
-    expected_secret = os.getenv('SXCTF_AUTH_SECRET')
+    try:
+        env = os.environ['DB_ENV']
+    except KeyError:
+        env = "prod"
 
-    if client_id == expected_id and client_secret == expected_secret:
-        jwt = 'j.w.t'
+    if env == 'local':
+        db_url = 'mysql+pymysql://saintsxctflocal:saintsxctf@db/saintsxctf'
     else:
-        jwt = ''
+        db_url = 'mysql+pymysql://test:test@localhost/saintsxctf'
 
-    return jsonify({'result': jwt})
+    engine = create_engine(db_url)
+
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+    session = Session()
+
+    email_pattern = re.compile('^(([a-zA-Z0-9_.-])+@([a-zA-Z0-9_.-])+\\.([a-zA-Z])+([a-zA-Z])+)?$')
+
+    if email_pattern.match(client_id):
+        user = session.query(User).filter_by(email=client_id).first()
+    else:
+        user = session.query(User).filter_by(username=client_id).first()
+
+    if user is None:
+        jwt_token = ''
+    elif bcrypt.checkpw(client_secret.encode('utf-8'), user.password.encode('utf-8')):
+        iat = int(datetime.utcnow().timestamp())
+        exp = iat + 3600
+
+        jwt_token = jwt.encode(
+            payload={
+                'iat': iat,
+                'exp': exp,
+                'iss': 'auth.saintsxctf.com',
+                'sub': user.username,
+                'email': user.email,
+                'name': f'{user.first} {user.last}'
+            },
+            key=private_key,
+            algorithm='RS256'
+        )
+    else:
+        jwt_token = ''
+
+    return jsonify({'result': jwt_token})
